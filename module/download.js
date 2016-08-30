aa3de3020e24ff605085bdd20f009dce7102a099
Dragfreeze <Dragfreeze@gmail.com>
2016-08-30 23:13:39 +0530

    cleanup download module

--- module/download.js
+++ module/download.js
@@ -1,21 +1,13 @@
 var request = require('request');
-var Stream = require('stream').Transform;
 var fs = require('fs');
 
 var download = function (uri, filename, callback, log){
-    request.head(uri, function(err, res, body){
-        // console.log('content-type:', res.headers['content-type']);
-        // console.log('content-length:', res.headers['content-length']);
-        try {
-            request(uri)
-                .pipe(fs.createWriteStream(filename))
-                .on('close', callback)
-                .on('error', function(err){console.log(err);});
-        } catch (e) {
-            console.log(e);
-        }
-        
-    });
+    request(uri)
+        .pipe(fs.createWriteStream(filename))
+        .on('close', callback)
+        .on('error', function(err){
+            console.log(err);
+        });
 };
 
 module.exports = download;
